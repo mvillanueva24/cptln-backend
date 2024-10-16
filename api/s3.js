b27e4197f4dd7a2b0894f2abc3c738ac336fab1c
JoseDEV799 <josedev799@gmail.com>
2024-10-15 21:22:48 -0500

    first commit

--- api/s3.js
+++ api/s3.js
@@ -1,48 +1,48 @@
-import { S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3'
-import { AWS_BUCKET_REGION, AWS_BUCKET_NAME, AWS_PUBLIC_KEY, AWS_SECRET_KEY } from './config.js'
-import fs from 'fs'
+// import { S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3'
+// import { AWS_BUCKET_REGION, AWS_BUCKET_NAME, AWS_PUBLIC_KEY, AWS_SECRET_KEY } from './config.js'
+// import fs from 'fs'
 
-const s3 = new S3Client({
-    region: AWS_BUCKET_REGION,
-    credentials: {
-        accessKeyId: AWS_PUBLIC_KEY,
-        secretAccessKey: AWS_SECRET_KEY
-    }
-})
+// const s3 = new S3Client({
+//     region: AWS_BUCKET_REGION,
+//     credentials: {
+//         accessKeyId: AWS_PUBLIC_KEY,
+//         secretAccessKey: AWS_SECRET_KEY
+//     }
+// })
 
-export async function upload(file) {
-    const stream = fs.createReadStream(file.tempFilePath)
-    const UploadParams = {
-        Bucket: AWS_BUCKET_NAME,
-        Key: file.name,
-        Body: stream
-    }
-    const command = new PutObjectCommand(UploadParams)
-    const result = await s3.send(command)
-    console.log(result);
-}
+// export async function upload(file) {
+//     const stream = fs.createReadStream(file.tempFilePath)
+//     const UploadParams = {
+//         Bucket: AWS_BUCKET_NAME,
+//         Key: file.name,
+//         Body: stream
+//     }
+//     const command = new PutObjectCommand(UploadParams)
+//     const result = await s3.send(command)
+//     console.log(result);
+// }
 
-export async function getItems() {
-    const command = new ListObjectsCommand({
-        Bucket: AWS_BUCKET_NAME
-    })
-    const result = await s3.send(command)
-    console.log(result)
-}
+// export async function getItems() {
+//     const command = new ListObjectsCommand({
+//         Bucket: AWS_BUCKET_NAME
+//     })
+//     const result = await s3.send(command)
+//     console.log(result)
+// }
 
-export async function getItem(filename) {
-    const command = new GetObjectCommand({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filename
-    })
-    return await s3.send(command)
-}
+// export async function getItem(filename) {
+//     const command = new GetObjectCommand({
+//         Bucket: AWS_BUCKET_NAME,
+//         Key: filename
+//     })
+//     return await s3.send(command)
+// }
 
-export async function getItem(filename) {
-    const command = new GetObjectCommand({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filename
-    })
-    const result = await s3.send(command)
-    result.Body.pipe(fs.createWriteStream(`./images/${filename}.jpg`))
-}
+// export async function getItem(filename) {
+//     const command = new GetObjectCommand({
+//         Bucket: AWS_BUCKET_NAME,
+//         Key: filename
+//     })
+//     const result = await s3.send(command)
+//     result.Body.pipe(fs.createWriteStream(`./images/${filename}.jpg`))
+// }
