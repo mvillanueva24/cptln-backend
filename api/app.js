758f57a0dda478d3831693d1c9fc0b78f353af79
JoseDEV799 <josedev799@gmail.com>
2024-10-16 22:20:18 -0500

    CORS

--- api/app.js
+++ api/app.js
@@ -9,6 +9,8 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 app.use(express.json())
 app.use(cookieParser())
