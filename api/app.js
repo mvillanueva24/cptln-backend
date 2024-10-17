90b39238512fa8fc8bede29c0aaaa47b31350c19
JoseDEV799 <josedev799@gmail.com>
2024-10-17 00:48:40 -0500

    CORS

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://felipe.josedev.net.pe','http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
