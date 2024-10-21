23aae9373f67e750ff96261479ed90a1870fba76
JoseDEV799 <josedev799@gmail.com>
2024-10-20 20:56:52 -0500

    Devocionales Optimizacion

--- api/app.js
+++ api/app.js
@@ -9,12 +9,12 @@ import cors from 'cors'
 
 const app = express()
 
-// app.use(cors({
-//     origin: 'https://felipe.josedev.net.pe',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }))
+app.use(cors({
+    origin: 'https://felipe.josedev.net.pe',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.use(fileUpload({
     useTempFiles: true,
