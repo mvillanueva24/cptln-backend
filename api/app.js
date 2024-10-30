9ebe01bf59a16b4ad3bf8081171c3582f591d3e1
JoseDEV799 <josedev799@gmail.com>
2024-10-30 00:23:47 -0500

    admin

--- api/app.js
+++ api/app.js
@@ -15,16 +15,15 @@ import cors from 'cors'
 const app = express()
 
 
-// app.use(cors({
-//     // origin: 'https://felipe.josedev.net.pe',
-//     origin: '192.168.0.6:5173',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }))
+app.use(cors({
+    origin: 'https://felipe.josedev.net.pe',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 
-app.use(cors())
+// app.use(cors())
 
 app.use(fileUpload({
     useTempFiles: true,
