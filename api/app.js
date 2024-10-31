d33611bf52dc737ade28de3bcc0eaaa4f56d6be4
JoseDEV799 <josedev799@gmail.com>
2024-10-30 22:56:14 -0500

    Programas

--- api/app.js
+++ api/app.js
@@ -23,7 +23,7 @@ const app = express()
 // }))
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
