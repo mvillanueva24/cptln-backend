222720b5beb93f2dd4c4e9a4cbbff71d9e0ac16a
JoseDEV799 <josedev799@gmail.com>
2024-10-30 23:34:00 -0500

    Programas

--- api/aws/s3.js
+++ api/aws/s3.js
@@ -8,14 +8,14 @@ config()
 const AWS_BUCKET_REGION=process.env.AWS_BUCKET_REGION
 const AWS_PUBLIC_KEY=process.env.AWS_PUBLIC_KEY
 const AWS_BUCKET_NAME=process.env.AWS_BUCKET_NAME
-const AWS_SECRET_KEY_CPTLN=process.env.AWS_SECRET_KEY_CPTLN
+const AWS_SECRET_KEY=process.env.AWS_SECRET_KEY
 
 // Comenzar instancia de S3
 const s3 = new S3Client({
     region: AWS_BUCKET_REGION,
     credentials: {
         accessKeyId: AWS_PUBLIC_KEY,
-        secretAccessKey: AWS_SECRET_KEY_CPTLN
+        secretAccessKey: AWS_SECRET_KEY
     }
 })
 
