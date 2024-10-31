69c90ca14b9da4b645963b53b65f3f72c1677b00
JoseDEV799 <josedev799@gmail.com>
2024-10-30 23:53:14 -0500

    Programas

--- api/app.js
+++ api/app.js
@@ -15,19 +15,19 @@ import cors from 'cors'
 const app = express()
 
 
-// app.use(cors({
-//     origin: 'https://felipe.josedev.net.pe',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }))
-
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://felipe.josedev.net.pe',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
+
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }))
 
 
 // app.use(cors())
