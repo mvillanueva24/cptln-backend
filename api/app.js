f45b6582c45694b56bc20b5bd67fb83fa393d19c
JoseDEV799 <josedev799@gmail.com>
2024-11-05 22:25:52 -0500

    Programas

--- api/app.js
+++ api/app.js
@@ -17,7 +17,6 @@ const app = express()
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
